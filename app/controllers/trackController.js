609972d40e38a6d75ca211cea93fee079e2bcaf7
Artur Skowronski <a.skowronski@smartrecruiters.com>
2015-06-28 03:20:39 +0200

    Hidden City

--- app/controllers/trackController.js
+++ app/controllers/trackController.js
@@ -10,12 +10,15 @@ module.exports = function (app) {
 
 router.get('/:deviceId/:contentId', function (req, res, next) {
 	console.log("" + req.params.contentId + ":" + req.params.deviceId )
-	Content.update({ _id: req.params.contentId }, { $push: { deviceIds: req.params.deviceId }},{upsert:true}, function (err) { 
+	Content.update({ _id: req.params.contentId }, { $push: { deviceIds: req.params.deviceId }},{upsert:true}, function (err, content) { 
 		if(err) {
-			console.log(err);
+			
+			res.status(404);
+
 		} else {
 			console.log("Successfully added");
+			res.status(200).json(content);
+
 		}
 	});
-	res.status(200).json(Content);
 });
