35a5a64161ea7c6a15ff52469f285548aa0ffcf3
Artur Skowronski <a.skowronski@smartrecruiters.com>
2015-06-28 04:34:16 +0200

    Track Controller

--- app/controllers/notificationController.js
+++ app/controllers/notificationController.js
@@ -12,13 +12,18 @@ module.exports = function (app) {
 
 function returnRandom(arr, device) {
   console.log("JSON"+ JSON.stringify(arr));
-  console.log("Device Id"+ device);
-  return arr.filter(function(item){
-    console.log("Device Id"+ item.deviceIds);
-    console.log("Item"+ JSON.stringify(item));
+  var contentIds = arr.map(function(item){
+    return item.content;
+  })
 
-    if(!item.deviceIds) return true;
-    return !item.deviceIds.includes(device)})[Math.floor(Math.random()*arr.length)];
+  Content.find({ _id: { $in: contentIds }, "deviceIds": {$ne:req.query.device}},function (err, content) {
+    if(err) {
+      return res.status(404).json({
+        message: "couldn't create object. Error: " + err
+      });
+    }
+    return status(200).json(content[Math.floor(Math.random()*content.length)]);
+  })
 }
 
 router.get('/:bid', function (req, res, next) {
@@ -34,18 +39,6 @@ router.get('/:bid', function (req, res, next) {
     if (relations !== null && relations.length > 0) {
       console.log(relations);
       var relation = returnRandom(relations, req.query.device);
-      console.log(relation);
-      var content = relation.content;
-      Content.findById(content,function (err, content) {
-        if (err) {
-          return res.status(500).json({
-            message: "couldn't create object. Error: " + err
-          });
-        }
-        res.status(200).json(
-          content
-          );
-      });
     } else {
       res.status(404).json({
         message: 'empty'
--- app/models/beaconContentRelation.js
+++ app/models/beaconContentRelation.js
@@ -1,16 +1,16 @@
 // Example model
 
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema,
-  shortid = require('shortid');
+Schema = mongoose.Schema,
+shortid = require('shortid');
 
 var BeaconContentRelationSchema = new Schema({
-  beacon: {
-    type: String, ref: 'Beacon', index: true, required: true
-  },
-  content: {
-    type: String, ref: 'ContentSchema', index: true, required: true
-  }
+	beacon: {
+		type: String, ref: 'Beacon', index: true, required: true
+	},
+	content: {
+		type: String, ref: 'ContentSchema', index: true, required: true
+	}
 });
 
 mongoose.model('BeaconContentRelation', BeaconContentRelationSchema);
